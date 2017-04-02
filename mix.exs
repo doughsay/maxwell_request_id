7e001c362fbee11ce17f5411dbab5c8e52b8ae2a
Chris Dosé <chris.dose@gmail.com>
2017-04-01 20:21:08 -0700

    Fix deps.

--- mix.exs
+++ mix.exs
@@ -12,25 +12,12 @@ defmodule MaxwellRequestId.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:maxwell, "~> 2"}]
+    [{:maxwell, ">= 2.2.0"}]
   end
 
   defp description do
